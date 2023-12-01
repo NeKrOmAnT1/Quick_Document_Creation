e2e250023089d670278d5be9113f92dcc256e562
Роман Мифтахов <miftahov9548@gmail.com>
2023-12-01 14:51:25 +0300

    Добавьте файлы проекта.

---
+++ Quick_Document_Creation/Models/Methods.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using Xceed.Words.NET;
+using Xceed.Document.NET;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+namespace Quick_Document_Creation.Models
+{
+    internal class Methods
+    {
+        public static void SelectDoc(ComboBoxItem selectItem, string Сourse, string Fio, string Groupe, ComboBoxItem Forma, string Date)
+        {
+            try
+            {
+                if (selectItem == null)
+                {
+                    MessageBox.Show("Не был выбран тип документа");
+                    return;
+                }
+
+                string select = selectItem.Tag.ToString();
+
+                switch (select)
+                {
+
+                case "Справка №1": GenerateDocMesto(Сourse, Fio, Groupe, Forma, Date); break;
+
+                }
+            }
+            catch(Exception) 
+            {
+                MessageBox.Show("Возникла ошибка");
+            }
+        }
+        public static void GenerateDocMesto(string Сourse, string Fio, string Groupe, ComboBoxItem Forma, string Date)
+        {
+            try
+            {
+                string FormaLessons = Forma.Tag.ToString();
+                using (var doc = DocX.Create($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\GeneratedDocument.docx"))
+                {
+                    var title = doc.InsertParagraph("СПРАВКА");
+                    title.Font("Times New Roman").FontSize(18).Bold().Alignment = Alignment.center;
+
+                    title.SpacingAfter(10); 
+
+                    var paragraph1 = doc.InsertParagraph($"Выдана {Fio}, {Date} числа," +
+                        $" в том, что он (она) действительно учится в ГАПОУ «СГК», на {Сourse} курсе в 2023-2024 учебном году. Специальность (код, наименование)" +
+                        $" {Groupe}, форма обучения: {FormaLessons}.");
+                    paragraph1.Font("Times New Roman").FontSize(14).SpacingAfter(10).Alignment = Alignment.both;
+
+                    var paragraph4 = doc.InsertParagraph($"Дата начала обучения: «_____» _____________ _____ г.");
+                    paragraph4.Font("Times New Roman").FontSize(14).SpacingAfter(10).Alignment = Alignment.both;
+
+                    var paragraph5 = doc.InsertParagraph($"Дата окончания обучения: «_____» _____________ _____ г.");
+                    paragraph5.Font("Times New Roman").FontSize(14).SpacingAfter(10).Alignment = Alignment.both;
+
+                    var paragraph6 = doc.InsertParagraph("Справка выдана по месту требования.");
+                    paragraph6.Font("Times New Roman").FontSize(14).SpacingAfter(10).Alignment = Alignment.both;
+
+                    var paragraph7 = doc.InsertParagraph("Директор __________________/ _____________________");
+                    paragraph7.Font("Times New Roman").FontSize(14).SpacingAfter(10).Alignment = Alignment.both;
+
+                    doc.Save();
+                }
+
+                MessageBox.Show("Документ успешно создан и сохранен.", "Готово");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка");
+            }
+        }
+    }
+}
---
+++ Quick_Document_Creation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Quick_Document_Creation.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand CreatDocCommand { get; set; }
+        public ICommand PrintDocCommand { get; set; }
+        public ComboBoxItem SelectDoc { get; set; }
+        public ComboBoxItem Forma { get; set; }
+        public string FIO { get; set; }
+        public string Date { get; set; }
+       
+        public string Group { get; set; }
+        public string Сourse { get; set; }
+         public MainWindowViewModel()
+         {
+            CreatDocCommand = new DelegateCommand(() => Models.Methods.SelectDoc(SelectDoc, Сourse, FIO, Group, Forma, Date ));
+         }
+    }
+}
